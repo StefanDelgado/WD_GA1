c9729b3428861b5f45e822e81ddf0535d29bfeff
StefanDelgado <99627352+StefanDelgado@users.noreply.github.com>
2023-09-26 12:55:01 +0800

    Original files

---
+++ CourseSmartgo/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="common.css"/>
+    <title></title>
+    </head>
+    <body>
+        <header>
+            <h1>CourseSmartgo</h1>
+        </header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="index.php">Courses</a>
+            <a href="index.php">About Us</a>
+            <a href="index.php">Contact Us</a>
+        </nav>
+    </body>
+</html>
